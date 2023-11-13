b43cddd6f697e87d4b5ed9484ac5b03851388a12
Lance Ward <Lanceward410@gmail.com>
2023-11-12 23:40:47 -0500

    Update start.bash
    'removed' chmod +x command temporarily

--- start.bash
+++ start.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "chmod +x ~/ROS_Setup/setup_ros.bash"
-chmod +x ~/ROS_Setup/setup_ros.bash
+#echo "chmod +x ~/ROS_Setup/setup_ros.bash"
+#chmod +x ~/ROS_Setup/setup_ros.bash
 echo "bash ~/ROS_Setup/setup_ros.bash"
 bash ~/ROS_Setup/setup_ros.bash
