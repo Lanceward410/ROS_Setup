6078737929ade8897edad5d409d8f68078ea9fa6
Lance Ward <Lanceward410@gmail.com>
2023-11-13 11:00:51 -0500

    Update start.bash
    Added auto-detect username, allows start.bash to assign permissions to setup_ros other than root, while running as sudo

--- start.bash
+++ start.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+username="$(whoami)"
 #echo "chmod +x ~/ROS_Setup/setup_ros.bash"
 #chmod +x ~/ROS_Setup/setup_ros.bash
-echo "bash ~/ROS_Setup/setup_ros.bash"
-bash ~/ROS_Setup/setup_ros.bash
+
+echo "Running setup_ros as $username"
+sudo -u $username bash ~/ROS_Setup/setup_ros.bash
