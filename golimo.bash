98bc4b7db72e0669504d13b80ccc0435d2f0d508
Lance Ward <Lanceward410@gmail.com>
2024-02-02 17:28:09 -0500

    Update golimo.bash
    temporary instructions for limo usage until bash file is fixed

--- golimo.bash
+++ golimo.bash
@@ -4,3 +4,17 @@
     gnome-terminal -- bash -c limostart.bash
     gnome-terminal -- bash -c limomap.bash
     gnome-terminal -- bash -c limocontrol.bash
+
+
+    cd ~/ugv_ws
+    source ~/.bashrc
+    roslaunch limo_gazebo_sim limo_ackerman.launch
+
+    cd ~/ugv_ws
+    source ~/.bashrc
+    rosparam set use_sim_time true
+    rosrun gmapping slam_gmapping scan:=/limo/scan
+
+    cd ~/ugv_ws
+    source ~/.bashrc
+    rosrun teleop_twist_keyboard teleop_twist_keyboard.py
