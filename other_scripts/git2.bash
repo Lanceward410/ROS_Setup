02d592ad17c92a3a6b1388541d4021e558ff400b
lanceward410 <lanceward410@gmail.com>
2024-04-21 15:01:22 -0400

    final changes to git2.bash, added pairkey function, added check for existing github ssh key file, added new comments

--- other_scripts/git2.bash
+++ other_scripts/git2.bash
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+# Function to pair SSH key (at the end)
+function pairkey() {
+  echo "Press Enter to continue to key pairing"
+  read -r
+  echo "Carrying on,"
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/$keyfilename
+  cat ~/.ssh/$keyfilename.pub | pbcopy
+  ssh -T git@github.com
+  git remote set-url origin git@github.com:Lanceward410/ROS_Setup.git
+}
+
 git config --global user.email "lanceward410@gmail.com"
 git config --global user.name "lanceward410"
 
 # Prompt the user to define a filename for the SSH key
-read -p "Enter the filename for the SSH key (without extension): " filename
+read -p "Enter the filename for the SSH key (without extension): " keyfilename
 
 # Generate SSH key with the specified filename
-ssh-keygen -t rsa -b 4096 -C "lanceward410@gmail.com" -f ~/.ssh/"$filename"
+ssh-keygen -t rsa -b 4096 -C "lanceward410@gmail.com" -f ~/.ssh/"$keyfilename"
 
 # Check if an SSH key already exists with the specified filename
-if [ -f ~/.ssh/"$filename" ]; then
-  echo "SSH key already exists at ~/.ssh/$filename."
+if [ -f ~/.ssh/"$keyfilename" ]; then
+  echo "SSH key already exists at ~/.ssh/$keyfilename."
   echo "If you proceed, the existing key will be overwritten."
   read -p "Do you want to continue? (y/n): " choice
   if [[ "$choice" != "y" ]]; then
@@ -20,12 +32,26 @@ if [ -f ~/.ssh/"$filename" ]; then
   fi
 fi
 
-# Display the public key
+# Concatenate key to easy-to-find file, and display it to user
+if [ -f ~/"Github_SSH_key.txt" ]; then
+  echo "SSH key already exists at ~/Github_SSH_key.txt."
+  echo "If you proceed, the existing key will be overwritten."
+  read -p "Do you want to continue? (y/n): " choice
+  if [[ "$choice" != "y" ]]; then
+    echo "Concatenation process aborted"
+    pairkey # Pairs key anyway, you just won't have it saved at ~/Github_SSH_key.txt
+    exit 1
+  fi
+fi
 echo "Your SSH public key is:"
 echo "Your SSH public key is:" >> ~/Github_SSH_key.txt
-cat ~/.ssh/"$filename.pub"
-cat ~/.ssh/"$filename.pub" >> ~/Github_SSH_key.txt
+cat ~/.ssh/"$keyfilename.pub"
+cat ~/.ssh/"$keyfilename.pub" >> ~/Github_SSH_key.txt
 
 # Instructions for adding the public key to GitHub
-echo "Add this public key to your GitHub account: https://github.com/settings/keys"
+echo "Please request that your public key get added to the main"
+echo "Github repository at: https://github.com/settings/keys"
 echo "You won't lose this key! It has been stored in ~/Github_SSH_key.txt"
+
+# Continue to pair your SSH
+pairkey
